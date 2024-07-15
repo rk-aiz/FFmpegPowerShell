c8234f65bf8414be11e6f1591f5f54a5c8f0acfd
rk-aiz <121715371+rk-aiz@users.noreply.github.com>
2024-07-15 18:11:19 +0900

    Fix ConsoleHelper

--- Script/FFmpeg_PowerShell_GUI.ps1
+++ Script/FFmpeg_PowerShell_GUI.ps1
@@ -29,7 +29,7 @@ Set-Location -LiteralPath $PSScriptRoot
 $global:path = $path.trim("`'")
 $global:parameters = $Parameters
 
-$OUTPUT_DIRECTORY = "D:\Encode"
+$OUTPUT_DIRECTORY = ""
 $OPTION_DIRECTORY = ""
 $OUTPUT_EXTENSION = ".mkv"
 
